61fb366d20ced4b21dd6516a62e0b265fb180571
Rounak <rounakkhandeparkar@gmail.com>
2021-03-13 13:37:43 +0530

    - Renamed class and deleted unused code.

--- app/src/androidTest/java/com/rdevs/techassignment/PrefsHelperTests.kt
+++ app/src/androidTest/java/com/rdevs/techassignment/PrefsHelperTests.kt
@@ -14,13 +14,7 @@ import org.junit.runner.RunWith
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 @RunWith(AndroidJUnit4::class)
-class ExampleInstrumentedTest {
-    @Test
-    fun useAppContext() {
-        // Context of the app under test.
-        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.rdevs.techassignment", appContext.packageName)
-    }
+class PrefsHelperTests {
 
 
     @Test
