78c62f82bf104dfa48f5f3687c82157cffd7e26d
Rounak <rounakkhandeparkar@gmail.com>
2021-03-13 13:36:15 +0530

    - Added Unit tests for PrefsHelper.

--- app/src/androidTest/java/com/rdevs/techassignment/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/rdevs/techassignment/ExampleInstrumentedTest.kt
@@ -1,12 +1,12 @@
 package com.rdevs.techassignment
 
-import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
-
+import androidx.test.platform.app.InstrumentationRegistry
+import com.rdevs.techassignment.utils.PrefsHelper
+import com.rdevs.techassignment.utils.UiUtils
+import org.junit.Assert.*
 import org.junit.Test
 import org.junit.runner.RunWith
-
-import org.junit.Assert.*
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -21,4 +21,37 @@ class ExampleInstrumentedTest {
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("com.rdevs.techassignment", appContext.packageName)
     }
+
+
+    @Test
+    fun testPreferencesHelper() {
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+
+        val prefsHelper: PrefsHelper = PrefsHelper.getInstance(appContext)
+
+        val TEST_KEY = "TEST_KEY"
+        var TEST_VALUE :String? = ""
+
+        prefsHelper.savePreferences(TEST_KEY, TEST_VALUE)
+        assertEquals(prefsHelper.getPreferences(TEST_KEY), "")
+
+        TEST_VALUE = "TEST_VALUE_1"
+        prefsHelper.savePreferences(TEST_KEY, TEST_VALUE)
+        assertEquals(prefsHelper.getPreferences(TEST_KEY), TEST_VALUE)
+
+        TEST_VALUE = "TEST_VALUE_2"
+        prefsHelper.savePreferences(TEST_KEY, TEST_VALUE)
+        assertNotEquals(prefsHelper.getPreferences(TEST_KEY), "TEST_VALUE")
+
+        TEST_VALUE = null
+        prefsHelper.savePreferences(TEST_KEY, TEST_VALUE)
+        assertNull(prefsHelper.getPreferences(TEST_KEY))
+
+        TEST_VALUE = "TEST_VALUE_3"
+        prefsHelper.savePreferences(TEST_KEY, TEST_VALUE)
+        assertNotNull(prefsHelper.getPreferences(TEST_KEY))
+
+    }
+
+
 }
